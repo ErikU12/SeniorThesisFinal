0d8644c145d9ed346bbc9a160788a6c4a5f9450b
JAMES\james <jamesbdog5@gmail.com>
2024-05-01 15:57:43 -0400

    no message

--- Assets/Scripts/Other/StartButton.cs
+++ Assets/Scripts/Other/StartButton.cs
@@ -1,10 +1,14 @@
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
-    public string sceneNameToLoad; // The name of the scene to load (set in the Inspector)
+    public SceneAsset sceneToLoad; // The scene to load (set in the Inspector)
+    public GameObject fadeImage; // Reference to the UI image for fading
+    public float fadeOutDuration = 1.5f; // Duration of the fade out effect
 
     private Button _button;
 
@@ -14,12 +18,45 @@ public class StartButton : MonoBehaviour
         _button = GetComponent<Button>();
 
         // Add a listener to the button's onClick event
-        _button.onClick.AddListener(LoadMainScene);
+        _button.onClick.AddListener(StartFadeOutAndLoadScene);
     }
 
-    private void LoadMainScene()
+    private void StartFadeOutAndLoadScene()
     {
-        // Load the specified scene
-        SceneManager.LoadScene(sceneNameToLoad);
+        if (sceneToLoad != null)
+        {
+            // Start the fade out process
+            StartCoroutine(FadeOutAndLoadScene());
+        }
+        else
+        {
+            Debug.LogError("Scene to load is not assigned!");
+        }
+    }
+
+    IEnumerator FadeOutAndLoadScene()
+    {
+        if (fadeImage != null)
+        {
+            float fadeOutTimer = 0f;
+            Color initialColor = fadeImage.GetComponent<Image>().color;
+            Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 1f); // Fade out to fully opaque
+
+            while (fadeOutTimer < fadeOutDuration)
+            {
+                fadeOutTimer += Time.deltaTime;
+                float normalizedTime = Mathf.Clamp01(fadeOutTimer / fadeOutDuration);
+                Color currentColor = Color.Lerp(initialColor, targetColor, normalizedTime);
+                fadeImage.GetComponent<Image>().color = currentColor;
+                yield return null;
+            }
+
+            // Load the specified scene after fade out completes
+            SceneManager.LoadScene(sceneToLoad.name);
+        }
+        else
+        {
+            Debug.LogError("Fade image reference is not set!");
+        }
     }
 }
